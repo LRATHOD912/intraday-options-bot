d53c6317589b2f3d4f70e7fe85c6f0dca61eff78
Lohith Rathod <rlohithtechie@gmail.com>
2026-07-08 03:54:00 -0400

    Add readiness threshold tests

--- app/tests/test_regime_thresholds.py
+++ app/tests/test_regime_thresholds.py
@@ -13,10 +13,20 @@ class TestRegimeThresholds(unittest.TestCase):
     def test_reversal_threshold(self):
         self.assertEqual(get_entry_quality_threshold("REVERSAL"), 60)
 
+    def test_reversal_59_fails_threshold_60(self):
+        self.assertLess(59, get_entry_quality_threshold("REVERSAL"))
+
+    def test_reversal_61_passes_even_though_static_75(self):
+        self.assertGreaterEqual(61, get_entry_quality_threshold("REVERSAL"))
+        self.assertLess(61, 75)
+
     def test_entry_quality_64_passes_choppy_but_fails_static_75(self):
         entry_quality = 64
         self.assertGreaterEqual(entry_quality, get_entry_quality_threshold("CHOPPY"))
         self.assertLess(entry_quality, 75)
+
+    def test_choppy_64_passes_threshold_62(self):
+        self.assertGreaterEqual(64, get_entry_quality_threshold("CHOPPY"))
 
     def test_entry_quality_64_fails_low_volatility(self):
         entry_quality = 64
